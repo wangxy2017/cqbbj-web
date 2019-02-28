c2ed54c2a95b9de91ded0566a2ff55c0ac8c26a8
刘磊 <3240600369@qq.com>
2019-02-28 21:23:13 +0800

    car

--- src/main/webapp/resources/js/car/car.js
+++ src/main/webapp/resources/js/car/car.js
@@ -35,13 +35,14 @@ layui.use(["table", "layer"], function () {
                     layer.msg("请选择一行数据");
                     return;
                 }
-                if (checkStatus.data.length > 1) {
-                    layer.msg("请单独选择一条进行删除");
-                    return;
+                var item="";
+                for(var i=0;i<checkStatus.data.length;i++){
+                    item = item+checkStatus.data[i].id+",";
                 }
+
                 layer.confirm("删除员工将放入回收站，确认删除吗？", function () {
                     // 请求后台，删除数据
-                    main.$http.post('/car/deleteActive', {"id": checkStatus.data[0].id}, {emulateJSON: true}).then(function (res) {
+                    main.$http.post('/car/deleteActive', {"ids": item.substring(0,item.length-1)}, {emulateJSON: true}).then(function (res) {
                         console.log(res.body);
                         if (res.body.code == 1) {
                             layer.msg("删除成功");
