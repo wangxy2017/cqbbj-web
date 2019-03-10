87abc99553571c146acf6236acf441e96c2bbd6b
王孝元 <243548880@qq.com>
2019-03-10 21:59:10 +0800

    跟进记录

---
+++ src/main/java/com/cqbbj/service/IIntentionFollowService.java
@@ -0,0 +1,15 @@
+package com.cqbbj.service;
+
+import com.cqbbj.core.base.BaseService;
+import com.cqbbj.entity.IntentionFollow;
+import com.cqbbj.entity.IntentionOrder;
+
+/**
+ * @author wangxy
+ * @version 1.0
+ * @ClassName: IIntentionFollowService
+ * @Description: 跟进记录业务层(这里用一句话描述这个类的作用)
+ * @date 2019/3/10 17:24
+ */
+public interface IIntentionFollowService extends BaseService<IntentionFollow> {
+}
