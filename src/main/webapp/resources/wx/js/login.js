ac01ed5bcf4c0c6f364aaa71ab7810d0be8f8d84
王孝元 <243548880@qq.com>
2019-03-28 19:41:41 +0800

    时间插件优化

--- src/main/webapp/resources/wx/js/login.js
+++ src/main/webapp/resources/wx/js/login.js
@@ -5,18 +5,20 @@ function doLogin() {
     // 验证为空
     if ($("#account").val() == 0 || $("#account").val() == null || $("#password").val() == 0 || $("#password").val() == null) {
         toastr.error("请输入完整");
-        $("#preloader").hide();
         return;
-    } else {
-        $("#preloader").show();
     }
-
     // 发送请求
     $.ajax({
         url: "/wx/login/empLogin",// 请求地址
         dataType: "JSON",// 返回数据类型
         data: {"account": $("#account").val(), "password": $("#password").val()},// 请求数据
         type: "POST",// 请求方式
+        beforeSend:function(){
+            $("#preloader").show();
+        },
+        complete:function(){
+            $("#preloader").hide();
+        },
         success: function (result) {// 请求成功函数
 
             if (result.code == 1) {
