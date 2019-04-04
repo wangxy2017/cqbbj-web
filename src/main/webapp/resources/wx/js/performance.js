1fce66e8739a5ddca9fbc42562aca86ef248420b
熊超 <2223051233@qq.com>
2019-04-04 20:19:20 +0800

    业绩查询统计UI界面

--- src/main/webapp/resources/wx/js/performance.js
+++ src/main/webapp/resources/wx/js/performance.js
@@ -18,10 +18,38 @@ var main = new Vue({
     }
 });
 
-var main = new Vue({
+var header = new Vue({
     el: "#header",
-    data: {},
+    data: {
+        emps:[],
+    },
     methods: {
+        /**
+         * 点击任意地方关闭弹窗
+         */
+        end: function () {
+            $(".alert_body").animate({
+                marginTop: '-100rem',
+            });
+            setTimeout(function () {
+                $(".alert_model").hide();
+            }, 500);
+        },
+        /**
+         * 点击关闭按钮动画
+         */
+        hide: function () {
+            $(".alert_body").animate({
+                marginTop: '-100rem',
+            });
+            setTimeout(function () {
+                $(".alert_model").hide();
+            }, 500);
+
+        },
+        /**
+         * 点击查询打开模态框
+         */
         vivw: function () {
 
             $("#header").animate({
@@ -29,14 +57,26 @@ var main = new Vue({
             });
         },
         /**
-         * 点击选择员工打开模态框
+         * 点击员工获取员工列表
          */
         open: function () {
-                $(".alert_model").show();
+            header.$http.post("/wx/employee/queryPageList", {"userKey": myCache.userKey}, {emulateJSON: true}).then(function (res) {
+                // console.log(res.body);
+                // [{name:xxx,emp_no:xxx},{name:xxx,emp_no:xxx},{name:xxx,emp_no:xxx}]
+                header.emps = res.body.data;
+            }, function (res) {
+
+            });
+
+            $(".alert_model").show();
 
             $(".alert_body").animate({
                 marginTop: '40rem',
             });
+
+        },
+        click: function () {
+            toastr.info("你选择了确定");
         }
     },
     /**
@@ -56,29 +96,7 @@ var main = new Vue({
     el: "#main",
     data: {},
     methods: {
-        /**
-         * 点击任意地方关闭弹窗
-         */
-        end: function () {
-            $(".alert_body").animate({
-                marginTop: '-50rem',
-            });
-            setTimeout(function () {
-                $(".alert_model").hide();
-            }, 500);
-        },
-        /**
-         * 点击关闭按钮动画
-         */
-        hide: function () {
-            $(".alert_body").animate({
-                marginTop: '-50rem',
-            });
-            setTimeout(function () {
-                $(".alert_model").hide();
-            }, 500);
 
-        }
 
     },
     mounted: function () {
